125b5f423e7d3e9883cf025d5fc8e0c4ae54903f
taro.test <taro.test@test.jp>
2018-05-31 12:12:38 +0000

    post

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+(1..5).each do |number|
+  Micropost.create(content: 'テスト ' + number.to_s, user_id: '2 ' + number.to_s)
+end
